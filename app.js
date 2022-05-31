84d609bfa018aaab20fae45074739386531cc7d9
Idris Westbrook <westbrook.idris@gmail.com>
2022-05-30 20:12:58 -0400

    add weather data as JSON

--- app.js
+++ app.js
@@ -14,5 +14,12 @@ var apiKey = config.API_KEY;
 
 function requestApi(city) {
     let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-    fetch(api).then(response => console.log(response.json()));
+    infoArea.innerText = 'Getting weather details...';
+    infoArea.classList.add('pending');
+    //Get API response and parse into JSON object, then call a function passing the API result as an argument
+    fetch(api).then(response => response.json()).then(result => weatherDetails(result));
+}
+
+function weatherDetails(info) {
+    console.log(info);
 }
