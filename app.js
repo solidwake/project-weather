fe028128df7600581a2e327c15e427884f2e7fd3
Idris Westbrook <westbrook.idris@gmail.com>
2022-05-30 22:15:45 -0400

    add return weather for device coordinates

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ infoArea = inputArea.querySelector('.info-area'),
 inputField = inputArea.querySelector('input'),
 locationButton = inputArea.querySelector('button');
 
+let api;
+
 inputField.addEventListener('keyup', event => {
     //Submit input data using 'enter' button if input field is not empty
     if(event.key == 'Enter' && inputField.value != '') {
@@ -21,17 +23,25 @@ locationButton.addEventListener('click', () => {
 });
 
 function onSuccess(position) {
-    console.log(position);
+    //Get latitude and longitude of the the user's device from the coords object
+    const {latitude, longitude} = position.coords;
+    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+    fetchData();
 }
 
 function onError(error) {
-    console.log(error);
+    infoArea.innerText = error.message;
+    infoArea.classList.add('error');
 }
 
 var apiKey = config.API_KEY;
 
 function requestApi(city) {
-    let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    fetchData();
+}
+
+function fetchData() {
     infoArea.innerText = 'Getting weather details...';
     infoArea.classList.add('pending');
     //Get API response and parse into JSON object, then call a function passing the API result as an argument
